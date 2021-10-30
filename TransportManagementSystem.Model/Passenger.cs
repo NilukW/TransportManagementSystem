81b6039a9a040fb1cad6d5a865385d995c74b0b6
Hafeez <hafeez.m@aeturnum.com>
2021-10-30 14:40:16 +0530

    Adding transport manager and passenger entities

--- TransportManagementSystem.Model/Passenger.cs
+++ TransportManagementSystem.Model/Passenger.cs
@@ -6,5 +6,9 @@ namespace TransportManagementSystem.Model
 {
     public class Passenger : User
     {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
     }
 }
--- TransportManagementSystem.Model/TransportManager.cs
+++ TransportManagementSystem.Model/TransportManager.cs
@@ -6,5 +6,11 @@ namespace TransportManagementSystem.Model
 {
     public class TransportManager : User
     {
+        public string FullName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Location { get; set; }
+        public string Gender { get; set; }
     }
 }
